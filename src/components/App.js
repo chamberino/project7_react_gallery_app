5f4d480ab6294e4ff8bf0c9f678f9d9746a9efbe
Michael Chambers <mchambers175@gmail.com>
2019-03-18 18:07:18 -0400

    set up directory and programmed data fetching from Flickr API

---
+++ src/components/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+// App Components
+import Nav from './Nav';
+import Search from './Search';
+import Gallery from './Gallery';
+import NotFound from './NotFound';
+
+import '../App.css';
+
+import apikey from './config.js'
+
+export default class App extends Component {
+  constructor() {
+    // calling super() allows us to use 'this' keyword in the context of the app class rather than the component class we're extending from react
+    super();
+    this.state = {
+      photos: [],
+      loading: true
+    };
+  }
+
+  // data fetching
+  componentDidMount() {
+    this.performSearch();
+  }
+
+  performSearch = (search = 'cats') => {
+    axios(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apikey}&tags=${search}&per_page=24&page=1&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({
+          photos: response.data.photos.photo,
+          loading: false
+        });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
+
+  // dogSearch = this.performSearch('dog');
+  // computerSearch = this.performSearch('computers');
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <Search onSearch={this.performSearch}/>
+          <Nav />
+          <Switch>
+            <Route exact path="/" render={ ()=>
+            (this.state.loading)
+              ? <p>Loading...</p>
+              : <Gallery photos={this.state.photos}/>} />
+
+            <Route path = '/cats'
+            render = { () => (this.state.loading)
+                ? <p> Loading...</p>
+                : <Gallery photos={this.state.photos} />} />
+
+            <Route path = '/dogs'
+            render = { () => (this.state.loading)
+                ? <p> Loading...</p>
+                : <Gallery photos={this.state.photos} />} />
+
+            <Route path = '/computers'
+            render = { () => (this.state.loading)
+                ? <p> Loading...</p>
+                : <Gallery photos={this.state.photos} />} />
+
+
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
---
+++ src/components/Gallery.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import GalleryItem from './GalleryItem'
+import NoResults from './NoResults'
+
+const Gallery = props => {
+
+  const images = props.photos;
+  let photos;
+  if (images.length > 0) {
+    photos = images.map(photo => <GalleryItem url= {`https:farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} key={photo.id} />)
+  } else {
+    photos = <NoResults />
+  }
+  return (
+    <div className="photo-container">
+      <h2>Results</h2>
+      <ul>
+        {photos}
+      </ul>
+    </div>
+  );
+}
+
+export default Gallery;
